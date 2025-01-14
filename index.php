175402d84ce62df998d5b49319d7a0e33a6e6202
ikerserna <146813295+ikerserna@users.noreply.github.com>
2025-01-14 15:41:43 +0100

    add index html + css

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+    <title>Projecte M7</title>
+</head>
+<body>
+
+    <form action="">
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username" required>
+        
+        <label for="username">Contrasenya</label>
+        <input type="text" name="contrasenya" placeholder="Contrasenya" required>
+
+        <button>Sign in</button>
+    </form>
+
+    <div>
+        <a href="">Don't have an account yet? <br> Sign up</a>
+    </div>
+
+</body>
+</html>
