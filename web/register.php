e60e363641e8cc79be6e9ae5429ceee529071582
niickbermudeez <146813170+niickbermudeez@users.noreply.github.com>
2025-01-14 16:05:21 +0100

    add register html + css

---
+++ web/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/register.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+    <title>Register</title>
+</head>
+<body>
+
+    <form action="">
+        <label for="name">First Name</label>
+        <input type="text" name="name" placeholder="Name" required>
+
+        <label for="lastname">Last Name</label>
+        <input type="text" name="lastname" placeholder="LastName" required>
+
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="Email" required>
+        
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username" required>
+
+        <label for="password">Password</label>
+        <input type="text" name="password" placeholder="Password" required>
+
+        <label for="verifypassword">Verify Password</label>
+        <input type="text" name="verifypassword" placeholder="VerifyPassword" required>
+
+        <button>Register</button>
+    </form>
+</body>
+</html>
