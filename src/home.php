90f34228feef4c088ffee48c2775579aecd5d3ea
ikerserna <146813295+ikerserna@users.noreply.github.com>
2025-01-28 16:12:05 +0100

    creación pagina home + css

---
+++ src/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barra de Menú Superior</title>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+
+    <header>
+        <nav class="navbar">
+            <div class="navbar-container">
+                <div class="logo">
+                    <img src="../img/logoTitulo.png" alt="Logo" id="logo">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                    </ul>
+                </div>
+                <div class="user-greeting">
+                    <p>Hola, <span id="user-name">Usuario</span>!</p>
+                </div>
+                <div class="hamburger" id="hamburger-menu">
+                    <span>&#9776;</span>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
